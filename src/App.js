59e423cdd95b8d5f71fe258773a699255f6f881b
Breyanna Vass <breyannavass@gmail.com>
2019-10-28 18:09:42 -0400

    using axios for the application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./App.css";
+import axios from "axios";
 
-function App() {
-  return <div className="App">Hello</div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      paras: 4,
+      html: true,
+      text: ""
+    };
+  }
+
+  componentDidMount() {
+    this.getSampleText();
+  }
+
+  getSampleText() {
+    axios.get("http://hipsterjesus.com/api?paras=" + this.state.paras);
+  }
+
+  render() {
+    return <div className="App">Hello</div>;
+  }
 }
 
 export default App;
